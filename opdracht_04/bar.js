2944e52f84b99cf2241e547e19e7b59bbe723ab6
Bardia <99704122@mydavinci.nl>
2023-03-20 14:15:44 +0100

    bar update

--- opdracht_04/bar.js
+++ opdracht_04/bar.js
@@ -6,51 +6,96 @@ const drinks = {
 
 let orders = [];
 
+const submitReceipt = () => {
+  groupOrders();
+  displayReceipt(orders);
+}
+
 const submitOrder = () => {
-  const orderInput = document.getElementById("orderInput");
-  const order = orderInput.value.toLowerCase();
-  if (order === "stop") {
-    displayReceipt();
-    return;
-  }
+  const order = prompt("Wat wilt u bestellen?").toLowerCase();
+  
   if (!drinks[order]) {
     alert("Sorry, we kennen dit drankje niet.");
-    return;
+  } 
+  
+  else {
+    const amount = prompt(`Hoeveel ${order} wilt u hebben?`);
+    addOrder(order, amount);
   }
-  const amount = prompt(`Hoeveel ${order} wilt u hebben?`);
+};
+
+const addOrder = (order, amount) => {
   orders.push({ name: order, amount: amount, price: drinks[order] });
 };
 
-const displayReceipt = () => {
+const groupOrders = () => {
+  let groups = {};
+  
+  orders.forEach(order => {
+    if (!groups[order.name]) {
+      groups[order.name] = {
+        name: order.name,
+        amount: Number(order.amount),
+        price: order.price
+      };
+    } else {
+      groups[order.name].amount += Number(order.amount);
+    }
+  });
+  
+  orders = Object.values(groups);
+};
+
+const displayReceipt = (orders) => {
   const receipt = document.getElementById("receipt");
   let totalCost = 0;
   receipt.innerHTML = "";
+  
   orders.forEach((order) => {
-    const row = document.createElement("tr");
-    const nameCell = document.createElement("td");
-    nameCell.innerHTML = order.name;
-    row.appendChild(nameCell);
-    const amountCell = document.createElement("td");
-    amountCell.innerHTML = `${order.amount}x`;
-    row.appendChild(amountCell);
-    const priceCell = document.createElement("td");
-    priceCell.innerHTML = `$${order.price}`;
-    row.appendChild(priceCell);
-    const totalCell = document.createElement("td");
-    totalCell.innerHTML = `$${order.amount * order.price}`;
-    row.appendChild(totalCell);
-    totalCost += order.amount * order.price;
+    const row = createTableRow(order.name, order.amount, order.price, order.price * order.amount);
     receipt.appendChild(row);
+    totalCost += order.price * order.amount;    
   });
+  
+  const totalRow = createTotalRow(totalCost);
+  receipt.appendChild(totalRow);
+};
+
+const createTableRow = (name, amount, price, total) => {
+  const row = document.createElement("tr");
+  const nameCell = document.createElement("td");
+  nameCell.innerHTML = name;
+  row.appendChild(nameCell);
+  
+  const amountCell = document.createElement("td");
+  amountCell.innerHTML = `${amount}x`;
+  row.appendChild(amountCell);
+  
+  const priceCell = document.createElement("td");
+  priceCell.innerHTML = `$${price}`;
+  row.appendChild(priceCell);
+  
+  const totalCell = document.createElement("td");
+  totalCell.innerHTML = `$${total}`;
+  row.appendChild(totalCell);
+  
+  return row;
+};
+
+const createTotalRow = (totalCost) => {
   const totalRow = document.createElement("tr");
+  
   const totalLabelCell = document.createElement("td");
   totalLabelCell.innerHTML = "Totaal";
   totalRow.appendChild(totalLabelCell);
+  
   const totalCell = document.createElement("td");
   totalCell.colSpan = "3";
   totalCell.innerHTML = `$${totalCost}`;
   totalRow.appendChild(totalCell);
-  receipt.appendChild(totalRow);
+  
+  return totalRow;
 };
 
-
+document.getElementById("bestelButton").addEventListener("click", submitOrder);
+document.getElementById("afrekenButton").addEventListener("click", submitReceipt);
