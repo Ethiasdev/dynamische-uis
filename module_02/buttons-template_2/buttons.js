d55e028dbe63ba907127594474f4f4d4d9ee5ab7
Ethiasdev <112874213+Ethiasdev@users.noreply.github.com>
2023-06-23 13:03:51 +0200

    update

--- module_02/buttons-template_2/buttons.js
+++ module_02/buttons-template_2/buttons.js
@@ -1,46 +1,49 @@
 function changeBackgroundAndImage() {
-    if (button1.innerHTML == 0 && button2.innerHTML == 0 && button3.innerHTML == 0) {
-        image.src = "images/bg0.jpg";
-        image1.src = "images/0.jpg";
-    } else if (button1.innerHTML == 1 && button2.innerHTML == 1 && button3.innerHTML == 1) {
-        image.src = "images/bg3.jpg";
-        image1.src = "images/3.jpg";
-    } else if (button1.innerHTML == 1 && button2.innerHTML == 1 || button1.innerHTML == 1 && button3.innerHTML == 1 || button2.innerHTML == 1 && button3.innerHTML == 1) {
-        image.src = "images/bg2.jpg";
-        image1.src = "images/2.jpg";
+    if (button1.style.backgroundColor === "red") {
+        image.src = "images/1.jpg";
+        image1.src = "images/bg1.jpg";
+    } else if (button2.style.backgroundColor === "red") {
+        image.src = "images/2.jpg";
+        image1.src = "images/bg2.jpg";
+    } else if (button3.style.backgroundColor === "red") {
+        image.src = "images/3.jpg";
+        image1.src = "images/bg3.jpg";
     } else {
-        image.src = "images/bg1.jpg";
-        image1.src = "images/1.jpg";
+        image.src = "images/0.jpg";
+        image1.src = "images/bg0.jpg";
     }
 }
 
 function increaseValue(button) {
-    if(button.style.backgroundColor == "red") return; 
+    if (button.style.backgroundColor === "red") return;
     var value = parseInt(button.innerHTML);
-    if (value == 1) {
-        value--;
-    } else {
-        value++;
-    }
+    value++;
+
     button.innerHTML = value;
-    button.style.backgroundColor = "red"; 
+    button.style.backgroundColor = "red";
+
+    if (button === button1) {
+        button2.style.backgroundColor = "";
+        button3.style.backgroundColor = "";
+    } else if (button === button2) {
+        button1.style.backgroundColor = "";
+        button3.style.backgroundColor = "";
+    } else if (button === button3) {
+        button1.style.backgroundColor = "";
+        button2.style.backgroundColor = "";
+    }
+
     changeBackgroundAndImage();
 }
 
 button1.addEventListener("click", function() {
     increaseValue(button1);
-    button2.style.backgroundColor = ""; 
-    button3.style.backgroundColor = "";
 });
 
 button2.addEventListener("click", function() {
     increaseValue(button2);
-    button1.style.backgroundColor = ""; 
-    button3.style.backgroundColor = "";
 });
 
 button3.addEventListener("click", function() {
     increaseValue(button3);
-    button1.style.backgroundColor = ""; 
-    button2.style.backgroundColor = "";
 });
